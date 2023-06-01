5f8d6a0ebd6edab4f17701eca2396b1fcaec252f
D1egox <diegourzua1311@gmail.com>
2023-06-01 04:00:55 -0400

    commit

--- register/Index.php
+++ register/Index.php
@@ -1,13 +1,14 @@
 <?php include '../src/header.php'; ?>
+<script src="../src/js/rut.js"></script>
 <main class="text-center row">
-  <form method='POST' class="col-3 mx-auto" required>
+  <form method='POST' class="col-3 mx-auto">
     <h1 class="mt-5 mb-5 display-1">Registro</h1>
-    <input type="text" name="rut" class="form-control mt-5 mb-5" placeholder="Rut">
-    <input type="text" name="nombre" class="form-control mt-5 mb-5" placeholder="Nombre" maxlength="30">
-    <input type="text" name="apellido" class="form-control mt-5 mb-5" placeholder="Apellido" maxlength="30">
-    <input type="Text" name="especialidad" class="form-control mt-5 mb-5" placeholder="Especialidad" maxlength="30">
-    <input type="email" name="email" class="form-control mt-5 mb-5" placeholder="Email">
-    <input type="password" name="pass" class="form-control mt-5 mb-5" placeholder="Clave" minlength="8">
+    <input type="text" name="rut" class="form-control mt-5 mb-5" placeholder="Rut" oninput="checkRut(this)" required>
+    <input type="text" name="nombre" class="form-control mt-5 mb-5" placeholder="Nombre" maxlength="30" required>
+    <input type="text" name="apellido" class="form-control mt-5 mb-5" placeholder="Apellido" maxlength="30" required>
+    <input type="Text" name="especialidad" class="form-control mt-5 mb-5" placeholder="Especialidad" maxlength="30" required>
+    <input type="email" name="email" class="form-control mt-5 mb-5" placeholder="Email" required>
+    <input type="password" name="pass" class="form-control mt-5 mb-5" placeholder="Clave" minlength="8" required>
     <button type="submit" class="btn btn-primary">Registrarse</button>
   </form>
 </main>

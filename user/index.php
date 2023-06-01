94825e6f23c52ce95ca2ae21f2a09a88829c589a
D1egox <diegourzua1311@gmail.com>
2023-06-01 04:58:37 -0400

    user

--- user/index.php
+++ user/index.php
@@ -1,8 +1,6 @@
 <?php include '../src/header.php'; ?>
-<?php include 'register.php'; ?>
-<script src="../src/js/rut.js"></script>
 <main class="text-center row">
     <h1 class="mt-5 mb-5 display-1">Bienvenido de nuevo user!</h1>
-    <img src="https://img.icons8.com/?size=512&id=65342&format=png">
+    <img class="col-2" src="https://img.icons8.com/?size=512&id=65342&format=png" width="50px">
 </main>
 <?php include '../src/footer.php'; ?>

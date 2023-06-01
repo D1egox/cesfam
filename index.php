b35215decc01655f3357d22d026ee55a22a4e25a
D1egox <diegourzua1311@gmail.com>
2023-06-01 03:41:26 -0400

    cree carpeta

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <?php include 'src/header.php'; ?>
 <main class="text-center">
   <h1 class="display-1">Cesfam</h1>
-  <a class="btn btn-primary" href="/register.php">Registrarse</a>
-  <a class="btn btn-danger" href="/login.php">Iniciar Sesion</a>
+  <a class="btn btn-primary" href="/register">Registrarse</a>
+  <a class="btn btn-danger" href="/login">Iniciar Sesion</a>
 </main>
 <?php include 'src/footer.php'; ?>

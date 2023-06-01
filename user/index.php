35acf7741faf80fac842a3a8144e33ca8bcdb643
oKidd <imnotokidd@gmail.com>
2023-06-01 04:51:21 -0400

    user template

---
+++ user/index.php
@@ -0,0 +1,8 @@
+<?php include '../src/header.php'; ?>
+<?php include 'register.php'; ?>
+<script src="../src/js/rut.js"></script>
+<main class="text-center row">
+    <h1 class="mt-5 mb-5 display-1">Bienvenido de nuevo user!</h1>
+    <img src="https://img.icons8.com/?size=512&id=65342&format=png">
+</main>
+<?php include '../src/footer.php'; ?>

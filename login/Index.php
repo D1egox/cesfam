3adffdeb34b423eca9f3b6431170f8b6689ead56
D1egox <diegourzua1311@gmail.com>
2023-06-01 03:52:41 -0400

    comii

--- login/Index.php
+++ login/Index.php
@@ -1,4 +1,4 @@
-<?php include 'src/header.php'; ?>
+<?php include '../src/header.php'; ?>
 <main class="text-center row">
   <form method='POST' class="col-3 mx-auto">
     <h1 class="mt-5 mb-5 display-1">Iniciar Sesion</h1>
@@ -7,4 +7,4 @@
     <button type="submit" class="btn btn-primary">Iniciar Sesion</button>
   </form>
 </main>
-<?php include 'src/footer.php'; ?>
+<?php include '../src/footer.php'; ?>
--- register/Index.php
+++ register/Index.php
@@ -1,4 +1,4 @@
-<?php include 'src/header.php'; ?>
+<?php include '../src/header.php'; ?>
 <main class="text-center row">
   <form method='POST' class="col-3 mx-auto" required>
     <h1 class="mt-5 mb-5 display-1">Registro</h1>
@@ -11,4 +11,4 @@
     <button type="submit" class="btn btn-primary">Registrarse</button>
   </form>
 </main>
-<?php include 'src/footer.php'; ?>
+<?php include '../src/footer.php'; ?>

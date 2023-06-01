6ac81b72da80cf77c3e0cd601333609311e7c28e
D1egox <diegourzua1311@gmail.com>
2023-06-01 05:20:52 -0400

    user

--- user/index.php
+++ user/index.php
@@ -1,14 +1,15 @@
 <?php include '../src/header.php'; ?>
+<?php session_start(); ?>
 <main class="text-center row">
-    <h1 class="mt-5 mb-5 display-1">Bienvenido de nuevo nombre apellido!</h1>
+    <h1 class="mt-5 mb-5 display-1">Bienvenido de nuevo <?php echo $_SESSION['nombre'], " ", $_SESSION['apellido']; ?></h1>
     <div class="col-8 mx-auto">
         <section class="col-2 float-start">
-            <img class="col" src="https://img.icons8.com/?size=512&id=65342&format=png">
+            <img class="col" src="https://img.icons8.com/?size=512&id=65342&format=png" width="150px">
         </section>
         <section class="col float-start text-start">
-            <p>Rut:</p>
-            <p>Email:</p>
-            <p>Especialidad:</p>
+            <p>Rut: <?php echo $_SESSION['rut']; ?></p>
+            <p>Email: <?php echo $_SESSION['email']; ?></p>
+            <p>Especialidad: <?php echo $_SESSION['especialidad']; ?></p>
         </section>
     </div>
 </main>

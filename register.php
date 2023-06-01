7c6e6f3a030e2dbd75507481f58dba5cae7a5bfc
D1egox <diegourzua1311@gmail.com>
2023-06-01 03:30:50 -0400

    primerCommit

--- register.php
+++ register.php
@@ -1,6 +1,6 @@
 <?php include 'src/header.php'; ?>
 <main class="text-center row">
-  <form method='POST' class="col-3 mx-auto">
+  <form method='POST' class="col-3 mx-auto" required>
     <h1 class="mt-5 mb-5 display-1">Registro</h1>
     <input type="text" class="form-control mt-5 mb-5" placeholder="Rut">
     <input type="text" class="form-control mt-5 mb-5" placeholder="Nombre">

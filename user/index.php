62f71354244ab58f6e721a2019ff582e35151d97
oKidd <imnotokidd@gmail.com>
2023-06-01 04:59:25 -0400

    user data

--- user/index.php
+++ user/index.php
@@ -1,6 +1,15 @@
 <?php include '../src/header.php'; ?>
 <main class="text-center row">
-    <h1 class="mt-5 mb-5 display-1">Bienvenido de nuevo user!</h1>
-    <img class="col-2" src="https://img.icons8.com/?size=512&id=65342&format=png" width="50px">
+    <h1 class="mt-5 mb-5 display-1">Bienvenido de nuevo nombre apellido!</h1>
+    <div class="col-8 mx-auto">
+        <section class="col-2 float-start">
+            <img src="https://img.icons8.com/?size=512&id=65342&format=png" width="50px">
+        </section>
+        <section class="col float-start text-start">
+            <p>Rut:</p>
+            <p>Email:</p>
+            <p>Especialidad:</p>
+        </section>
+    </div>
 </main>
 <?php include '../src/footer.php'; ?>

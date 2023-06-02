c41c14ead37464124cd2c6ad14a066a8d665d3b2
oKidd <imnotokidd@gmail.com>
2023-06-02 02:24:02 -0400

    commit

--- src/js/agregarFila.js
+++ src/js/agregarFila.js
@@ -3,6 +3,7 @@ var container = document.getElementById('container');
 
 function agregarFila() {
   var clonedDiv = document.createElement('div');
+  console.log(divisionOriginal.innerHTML);
   clonedDiv.innerHTML = divisionOriginal.innerHTML;
   container.appendChild(clonedDiv);
 };
